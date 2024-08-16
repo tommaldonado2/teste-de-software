98a11abae4aabbbad72fb2039120f9492dd00bbc
tommaldonado2 <tommaldonado2@gmail.com>
2024-08-16 16:13:55 -0300

    Adicionado atributo data-test titulo-principal ao h1 e atualizado o seletor do teste

--- testes-e2e-cypress-main/cypress/e2e/spec.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/spec.cy.js
@@ -1,6 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('h1').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+describe('Página inicial', () => {
+  // beforeEach( () => {
+  //   cy.visit('http://localhost:3000/')
+  // })
+
+  it('Deve renderizar h1 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('[data-test="titulo-principal"]').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
   })
 })
